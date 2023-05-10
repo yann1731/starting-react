4ac424e835c6140a0024b7db989b84d624aa36f9
yannick <yan_1731@hotmail.com>
2023-05-10 12:37:22 -0400

    Learning about react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import pokemon from "./pokemon.json"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}>
+      <h1 className={"title"}>Pokemon Search</h1>
+      <table width={"100%"}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+        {pokemon.map((pokemon) => (
+            <tr key={pokemon.id}>
+                <th>{pokemon.name.english}</th>
+                <th>{pokemon.type.join(", ")}</th>
+            </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
